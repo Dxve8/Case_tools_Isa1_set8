181a7e0224132c22f7dda911f35035fb3d0d2992
Dave Goes <goesdave.12@@gmail.com>
2024-02-12 09:04:15 +0530

    commited

--- src/AsciiValue.java
+++ src/AsciiValue.java
@@ -3,7 +3,12 @@ public class AsciiValue {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        
+		char ch='a';
+		int ascii=ch;
+		int castAscii =(int)ch;
+		System.out.println("The Ascii VAlue of "+ch+"is "+ascii);
+		System.out.println("The Ascii VAlue of "+ch+"is "+castAscii);
 	}
 
 }
